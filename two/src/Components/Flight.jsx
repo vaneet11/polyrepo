ccc5a8c494984e6343039d19a00078670e2d8611
Sunder <sunder@silstonegroup.com>
2023-09-11 20:58:09 +0530

    ini

--- two/src/Components/Flight.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Flight() {
-    return (
-        <div>Flight</div>
-    )
-}
--- two/src/utils/auth.jsx
+++
@@ -1,7 +0,0 @@
-export const jwt_secret = "AirBook_JS"
-
-
-export const isLoggedIn = () => {
-    const tokenExist = localStorage.getItem('token')
-    return tokenExist ? true : false
-}
